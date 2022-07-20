370a60659a43e43d6521d41d9277d25b1f9ca793
Abidemi Ope <opeabidemi@gmail.com>
2022-07-20 14:19:32 +0100

    new class

---
+++ src/com/abidemiope/Main.java
@@ -0,0 +1,31 @@
+package com.abidemiope;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("I'm a bad guy"); // Cap
+        /* The code below will print the words Hello World
+            to the screen, and it is amazing */
+        String name = "James Bond";
+        int age = 45;
+        float PI = 3.142f;
+        double rad = 45.373743;
+        final char gender = 'M';
+        final boolean isDark = false;
+
+        //  gender = 'F';
+
+        age = 35;
+
+        int a, b;
+        a = 20; b= 40;
+
+        int c = a + b;
+
+        int intRad = (int) rad;
+
+        String greeting = "Hello";
+        String sentence = greeting + " " + name;
+
+        System.out.println(sentence);
+    }
+}
